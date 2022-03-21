69d57e9e1ffa077691b5e9958536048c52a55112
M B <85039141+m-bartlett@users.noreply.github.com>
2022-03-20 19:59:17 -0600

    move read Xresources function to synonymous source files

--- Xresources.h
+++ Xresources.h
@@ -3,10 +3,12 @@
 
 #include <xcb/xcb.h>
 #include <xcb/xcb_xrm.h>
+#include "userdata.h"
 
 void  Xresource_init();
 void  Xresource_close();
 char* Xresource_get(char* key);
+void read_from_Xresources(userdata_t *userdata);
 
 
 #define XRESOURCE_KEY_ICON_PRIMARY_COLOR   "pavol-dunst.primaryColor"
