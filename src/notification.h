ab590275ed0cfbfe204f79e8d7031b1bd50b211b
M B <85039141+m-bartlett@users.noreply.github.com>
2022-03-21 09:41:15 -0600

    remove deprecated icon definitions

--- src/notification.h
+++ src/notification.h
@@ -9,14 +9,6 @@
 #define NOTIFICATION_LITERAL_HINT_SYNCHRONOUS "synchronous"
 #define NOTIFICATION_LITERAL_HINT_VALUE       "value"
 
-#define ICON_FILE_PREFIX        "audio-volume-"
-#define ICON_FILE_SUFFIX        "-symbolic.svg"
-#define ICON_FILE_MUTED         ICON_FILE_PREFIX "muted"         ICON_FILE_SUFFIX
-#define ICON_FILE_LOW           ICON_FILE_PREFIX "low"           ICON_FILE_SUFFIX
-#define ICON_FILE_MEDIUM        ICON_FILE_PREFIX "medium"        ICON_FILE_SUFFIX
-#define ICON_FILE_HIGH          ICON_FILE_PREFIX "high"          ICON_FILE_SUFFIX
-#define ICON_FILE_OVERAMPLIFIED ICON_FILE_PREFIX "overamplified" ICON_FILE_SUFFIX
-
 extern const char* NOTIFICATION_BODY_FORMAT;
 extern const int   NOTIFICATION_BODY_FORMAT_SIZE;
 
