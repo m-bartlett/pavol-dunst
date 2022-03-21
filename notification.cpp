09f47a9a55b394eacf2fc4d0543bb0a39f47cf69
M B <85039141+m-bartlett@users.noreply.github.com>
2022-03-20 20:01:06 -0600

    refactor variable names to avoid ambiguity with --icon-size

--- notification.cpp
+++ notification.cpp
@@ -6,18 +6,18 @@
 const char* NOTIFICATION_BODY_FORMAT = NOTIFICATION_LITERAL_BODY_FORMAT;
 const int   NOTIFICATION_BODY_FORMAT_SIZE = strlen(NOTIFICATION_BODY_FORMAT) - 2;
 
-const guint8* icons[] = { (guint8*) silent_svg_raw,
-                          (guint8*)    low_svg_raw,
-                          (guint8*) medium_svg_raw,
-                          (guint8*)   high_svg_raw };
+const guint8* icon_bodies[] = { (guint8*) silent_svg_raw,
+                                (guint8*)    low_svg_raw,
+                                (guint8*) medium_svg_raw,
+                                (guint8*)   high_svg_raw };
 
-const gsize icon_sizes[] = { (gsize) silent_svg_raw_size,
-                             (gsize)    low_svg_raw_size,
-                             (gsize) medium_svg_raw_size,
-                             (gsize)   high_svg_raw_size };
+const gsize icon_body_sizes[] = { (gsize) silent_svg_raw_size,
+                                  (gsize)    low_svg_raw_size,
+                                  (gsize) medium_svg_raw_size,
+                                  (gsize)   high_svg_raw_size };
 
-const size_t icons_size = sizeof(icons) / sizeof(icons[0]);
-const unsigned char divisor = 100 / icons_size;
+const size_t icon_bodies_size = sizeof(icon_bodies) / sizeof(icon_bodies[0]);
+const unsigned char divisor = 100 / icon_bodies_size;
 
 void display_volume_notification(userdata_t *userdata) {
   int volume = userdata->new_volume;
@@ -25,25 +25,20 @@ void display_volume_notification(userdata_t *userdata) {
   printf("%d\n", volume);
   fflush(stdout);
 
-  char *summary = (char*)NOTIFICATION_LITERAL_CATEGORY;
-  guint8* icon    = (guint8*) high_svg_raw;
-  gsize icon_size = (gsize)   high_svg_raw_size;
+  char *summary        = (char*)NOTIFICATION_LITERAL_CATEGORY;
+  guint8* icon_body    = (guint8*) high_svg_raw;
+  gsize icon_body_size = (gsize)   high_svg_raw_size;
 
-  if (userdata->mute == MUTE_ON) {
-    icon      = (guint8*) muted_svg_raw;
-    icon_size = (gsize)   muted_svg_raw_size;
-  }
+  if (userdata->mute == MUTE_ON) { icon_body      = (guint8*) muted_svg_raw;
+                                   icon_body_size = (gsize)   muted_svg_raw_size; }
   else {
     unsigned char loudness = volume ? (volume-1)/divisor : 0;
-    if (loudness < icons_size) {
-      icon      = (guint8*) icons[loudness];
-      icon_size = (gsize)   icon_sizes[loudness];
-    }
-    else {
-      icon      = (guint8*) overamplified_svg_raw;
-      icon_size = (gsize)   overamplified_svg_raw_size;
-      volume = (volume % 100) * 100 / PULSEAUDIO_OVERAMPLIFIED_RANGE;
-    }
+    if (loudness < icon_bodies_size) { icon_body      = (guint8*) icon_bodies[loudness];
+                                       icon_body_size = (gsize)   icon_body_sizes[loudness]; }
+
+    else { icon_body      = (guint8*) overamplified_svg_raw;
+           icon_body_size = (gsize)   overamplified_svg_raw_size;
+           volume         = (volume % 100) * 100 / PULSEAUDIO_OVERAMPLIFIED_RANGE; }
   }
 
   size_t body_width = NOTIFICATION_BODY_FORMAT_SIZE + strlen(userdata->notification_body)+1;
@@ -55,16 +50,15 @@ void display_volume_notification(userdata_t *userdata) {
   notify_notification_set_category(notification, NOTIFICATION_LITERAL_CATEGORY);
   notify_notification_set_timeout(notification,  userdata->notification_timeout);
 
-  bool icon_success = populate_notification_icon(notification, icon, icon_size,
-                                                 userdata->icon_primary_color,
-                                                 userdata->icon_secondary_color);
-  if (!icon_success) {
-    fprintf(stderr, "Error rendering notification icon\n" );
-    exit(EXIT_FAILURE);
-  }
+  bool icon_success = render_notification_icon(notification, icon_body, icon_body_size,
+                                               userdata->icon_size,
+                                               userdata->icon_primary_color,
+                                               userdata->icon_secondary_color);
+  if (!icon_success) { fprintf(stderr, "Error rendering notification icon\n");
+                       exit(EXIT_FAILURE); }
 
   /* https://people.gnome.org/~desrt/glib-docs/glib-GVariant.html
-     info on GVariants since notify_notification_set_hint_* methods are deprecated */
+        info on GVariants since notify_notification_set_hint_* methods are deprecated */
 
   // Set notification synchronous (overwrite existing notification instead of making a fresh one)
   notify_notification_set_hint( notification,
